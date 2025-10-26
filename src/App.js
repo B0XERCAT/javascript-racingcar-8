cb874ffd7e8110eec6c965aedb0ee9c5a5898291
B0XERCAT <seojin3154@naver.com>
2025-10-27 06:48:02 +0900

    refactor: rename variables and functions for readability

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 const SINGLE_DIGIT = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-async function readCarsInput() {
+async function readCarNames() {
   return await Console.readLineAsync(
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
   );
 }
 
-async function readCountInput() {
+async function readRoundCount() {
   return await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 }
 
-function extractCars(input) {
+function parseCarNames(input) {
   return input.split(",").map((name) => name.trim());
 }
 
-function initializeRace(cars) {
+function initializeDistances(cars) {
   return Array.from({ length: cars.length }, () => 0);
 }
 
@@ -29,27 +29,30 @@ function updateDistances(distances) {
   return distances.map((distance) => distance + moveCar());
 }
 
-function runRace(count, initialDistances) {
-  const finalDistances = Array.from({ length: count }).reduce((distances) => {
-    const newDistances = updateDistances(distances);
-    // TODO: print each round
-    return newDistances;
-  }, initialDistances);
+function runRace(roundCount, initialDistances) {
+  const finalDistances = Array.from({ length: roundCount }).reduce(
+    (distances) => {
+      const newDistances = updateDistances(distances);
+      // TODO: print each round
+      return newDistances;
+    },
+    initialDistances
+  );
 
   return finalDistances;
 }
 
 class App {
   async run() {
-    const carsInput = await readCarsInput();
-    const countInput = await readCountInput();
+    const carNamesInput = await readCarNames();
+    const roundCountInput = await readRoundCount();
 
-    const cars = extractCars(carsInput);
-    const count = Number(countInput);
+    const carNames = parseCarNames(carNamesInput);
+    const roundCount = Number(roundCountInput);
 
-    const initialDistances = initializeRace(cars);
+    const initialDistances = initializeDistances(carNames);
 
-    const finalDistances = runRace(count, initialDistances);
+    const finalDistances = runRace(roundCount, initialDistances);
 
     // TODO: determin winner and print
   }
