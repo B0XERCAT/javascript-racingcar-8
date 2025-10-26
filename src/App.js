a5631df9efef88dd28e6151ce1bac9b94a765268
B0XERCAT <seojin3154@naver.com>
2025-10-26 17:02:55 +0900

    feat: extract cars from user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
+async function readUserInput() {
+  return await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+}
+
+function extractCars(input) {
+  return input.split(",").map((name) => name.trim());
+}
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await readUserInput();
+    const cars = extractCars(userInput);
+  }
 }
 
 export default App;
