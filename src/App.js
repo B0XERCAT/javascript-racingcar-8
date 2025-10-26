7ee03505edd330f330609932084de8f3bfd059fa
B0XERCAT <seojin3154@naver.com>
2025-10-27 06:48:24 +0900

    fix: remove 10 from single digit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-const SINGLE_DIGIT = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const SINGLE_DIGIT = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 async function readCarNames() {
   return await Console.readLineAsync(
